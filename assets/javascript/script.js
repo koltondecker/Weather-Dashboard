208d5d3462cd057d0f629bb6bf449f30ebf8efd9
Kolton Decker <deckerman25@gmail.com>
2021-01-15 15:36:18 -0600

    Page now displays information from 'Current Weather Data' API on the page. This includes city name, temperature, humidity, and wind speed.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,76 +1,91 @@
 $(document).ready(function() {
 //Javascript code in here.
 
-var apiKey = "c67bcac9baf63e1d77cd3d4a1d20a93c";
-var recentCitiesArray = [];
-var newCity;
+    var apiKey = "c67bcac9baf63e1d77cd3d4a1d20a93c";
+    var recentCitiesArray = [];
+    var newCity;
+    var recentSearchesAmount = 10;
 
-addCityToList();
+    updateListofCities();
 
-$("#searchBtn").on("click", function(e) {
-    e.preventDefault();
-    newCity = $("#search-city").val().trim();
+    $("#searchBtn").on("click", function(e) {
+        e.preventDefault();
+        newCity = $("#search-city").val().trim();
 
-    addCityToList(newCity);
-    weatherAPI(newCity);
-});
+        updateListofCities(newCity);
+        weatherAPI(newCity);
+    });
 
 
-function addCityToList(newCity) {
-    //! When adding recently searched cities to list on left of page, MAKE SURE to give each list item (li) a class of "list-group-item"!!!
-    //Parse local storage for recent cities list array
-    //save that to our recentCitiesArray
-    //Prepend newCity value to the front of our array and slice off last value if array exceeds length of 10
-    //Use a for loop the length of the array to create <li> items and append those to #recent-cities-list div
-    $("#recent-cities-list").empty();
+    function updateListofCities(newCity) {
 
-    var recentCities = JSON.parse(localStorage.getItem("recentCitiesArray"));
+        //Clear list of recent cities each time so we aren't stacking content
+        $("#recent-cities-list").empty();
 
-    if(recentCities !== null) {
-        recentCitiesArray = recentCities;
-    }
+        //Parse local storage for recent cities list array.
+        var recentCities = JSON.parse(localStorage.getItem("recentCitiesArray"));
 
-    if(newCity !== undefined) {
-        var index = recentCitiesArray.indexOf(newCity);
+        //Save local storage to our recentCitiesArray if it isn't empty.
+        if(recentCities !== null) {
+            recentCitiesArray = recentCities;
+        }
 
-        if (index >= 0) {
-            recentCitiesArray.splice(index, 1);
+        //Check if city already exists in array, slice it out if it does, then prepend newCity value to the front of our array.
+        if(newCity !== undefined) {
+            var index = recentCitiesArray.indexOf(newCity);
+
+            if (index >= 0) {
+                recentCitiesArray.splice(index, 1);
+            }
+            
+            recentCitiesArray.unshift(newCity);
         }
         
-        recentCitiesArray.unshift(newCity);
+        //Slices off the last element in the array if the array exceeds a length of 10;
+        if(recentCitiesArray.length > recentSearchesAmount) {
+            recentCitiesArray.pop();
+        }
+
+        //For loop the length of the array to create <li> items and append those to #recent-cities-list div
+        for(i = 0; i < recentCitiesArray.length; i++) {
+            var newLi = $("<li></li>");
+
+            newLi.addClass("list-group-item");
+            newLi.text(recentCitiesArray[i]);
+
+            $("#recent-cities-list").append(newLi);
+        }
+
+        localStorage.setItem("recentCitiesArray", JSON.stringify(recentCitiesArray));
+
     }
-    
 
-    if(recentCitiesArray.length > 10) {
-        recentCitiesArray.pop();
+    function weatherAPI(newCity) {
+        $("#city-current-info").empty();
+        $("#city-5-day-forecast").empty();
+
+        var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + newCity + "&units=imperial&&appid=" + apiKey;
+
+        $.ajax({
+            url: queryUrl,
+            method: "Get"
+        }).then(function(response) {
+            console.log(response);
+
+            var cityName = $("<h3>");
+            cityName.text(response.name);
+            var temperature = $("<p>");
+            temperature.text("Temperature: " + response.main.temp + " ºF");
+            var humidity = $("<p>");
+            humidity.text("Humidity: " + response.main.humidity + "%");
+            var windSpeed = $("<p>");
+            windSpeed.text("Wind Speed: " + response.wind.speed + " MPH");
+            
+            $("#city-current-info").append(cityName, temperature, humidity, windSpeed);
+
+        })
+
     }
-
-    for(i = 0; i < recentCitiesArray.length; i++) {
-        var newLi = $("<li></li>");
-        newLi.addClass("list-group-item");
-        newLi.text(recentCitiesArray[i]);
-        console.log(newLi);
-
-        $("#recent-cities-list").append(newLi);
-    }
-
-    localStorage.setItem("recentCitiesArray", JSON.stringify(recentCitiesArray));
-
-}
-
-function weatherAPI(newCity) {
-
-    var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + newCity + "&appid=" + apiKey;
-
-    $.ajax({
-        url: queryUrl,
-        method: "Get"
-    }).then(function(response) {
-        console.log(response);
-    })
-
-}
-
 
 
 
